aa79278eaa2e278a7f920803d7b4ecaae2f7229d
Muhammad Sahal <taharimbadhon27@gmail.com>
2026-08-04 13:10:04 +0600

    Update CastleTvProviderPlugin.kt

--- CastleTvProvider/src/main/kotlin/com/cncverse/CastleTvProviderPlugin.kt
+++ CastleTvProvider/src/main/kotlin/com/cncverse/CastleTvProviderPlugin.kt
@@ -5,10 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class CastleTvProviderPlugin: Plugin() {
+class CastleTvProviderPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        CastleTvProvider.context = context
         registerMainAPI(CastleTvProvider())
     }
 }
