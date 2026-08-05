554d2efcdf398f52884454aa97ce0b18dd316819
Muhammad Sahal <taharimbadhon27@gmail.com>
2026-08-05 15:09:18 +0600

    Update liveprovider.kt

--- LiveTv/src/main/kotlin/com/cncverse/liveprovider.kt
+++ LiveTv/src/main/kotlin/com/cncverse/liveprovider.kt
@@ -1,11 +1,1 @@
-package com.cncverse
-
-import com.lagradost.cloudstream3.plugins.BasePlugin
-import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-
-@CloudstreamPlugin
-class LivePlugin : BasePlugin() {
-    override fun load() {
-        registerMainAPI(LiveProvider())
-    }
-}
+//
